56ced4f33090d5536cba041619d623f4d9bf50a5
Nicolas Thiebaut <nicolas.thiebaut@hired.com>
2020-03-06 15:42:13 -0800

    :sparkles: add arXiv trackback submission script

---
+++ submit-arxiv-trackback.sh
@@ -0,0 +1,3 @@
+# From https://isomorphismes.tumblr.com/post/1110077044/trackback
+curl -d url="https://data4thought.com/lime-of-words.html" -d title="Lime of Words" -d blog_name="Data4thought" -d excerpt="This blog post shares experiments on the LIME (Local Interpretable Model-agnostic Explanations) interpretation model." https://arxiv.org/trackback/gr-qc/9712019
+
