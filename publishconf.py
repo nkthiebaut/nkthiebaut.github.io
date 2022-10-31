b7fc09b0716441ce0d8b4ed36116a4e465332bb4
Nicolas Thiebaut <nicolas.thiebaut@hired.com>
2022-10-31 00:19:28 -0700

    Publish pelican content

--- publishconf.py
+++ publishconf.py
@@ -15,6 +15,9 @@ RELATIVE_URLS = False
 
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_MAX_ITEMS = 15
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
